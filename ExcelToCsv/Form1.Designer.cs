1f4ce4080565f56252d12025d7ca5b273c394e80
Тончевски Христијан <hristijan.tonchevski@students.finki.ukim.mk>
2022-06-30 21:48:52 +0200

    Add project files.

---
+++ ExcelToCsv/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ExcelToCsv
+{
+    partial class ExcelToCsv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(780, 353);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(416, 390);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(163, 48);
+            this.btnChoose.TabIndex = 1;
+            this.btnChoose.Text = "Import";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(605, 390);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(163, 48);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.label1.Location = new System.Drawing.Point(9, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Import File Name: Import.xlsx";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.label2.Location = new System.Drawing.Point(12, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Export File Name: Export.csv";
+            // 
+            // ExcelToCsv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ExcelToCsv";
+            this.Text = "ExcelToCsv";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExcelToCsv/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace ExcelToCsv
+{
+    public partial class ExcelToCsv : Form
+    {
+        public ExcelToCsv()
+        {
+            InitializeComponent();
+        }
+     
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            ExcelApp.Application excelApp = new ExcelApp.Application();
+            ExcelApp.Workbook excelBook = excelApp.Workbooks.Open("C:\\CSV\\Import.xlsx"); // Excel Document
+            ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+            ExcelApp.Range excelRange = excelSheet.UsedRange;
+
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+
+            dataGridView1.RowCount = rows;
+            dataGridView1.ColumnCount = cols;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= cols; j++)
+                {
+                    if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
+                        dataGridView1.Rows[i - 1].Cells[j - 1].Value = excelRange.Cells[i, j].Value2.ToString();
+                }
+            }
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            string csv = string.Empty;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    csv += "\"" + cell.Value + "\"";
+                    csv += ",";
+                }
+                csv += "\"\",\"\",\"\"";
+                csv += "\r\n";
+            }
+            string result = csv.Remove(csv.TrimEnd().LastIndexOf(Environment.NewLine));
+            string folderPath = "C:\\CSV\\"; // CSV Export Document
+            File.WriteAllText(folderPath + "Export.csv", result);
+        }
+    }
+}
